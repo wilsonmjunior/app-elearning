55030bb4f3e411606954ec17156a294ab2091df3
wilsonmjunior <wilsonmjuniorx@gmailcom>
2020-12-19 18:06:15 -0300

    adjusted the pages

---
+++ src/pages/CoursesSaves/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const CoursesSaves = () => (
+  <View>
+    <Text>Courses Saves</Text>
+  </View>
+)
+
+export default CoursesSaves
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
+import { View, Text } from 'react-native'
 
-const Home = () => {}
+const Home = () => {
+  return (
+    <View>
+      <Text>Delta</Text>
+    </View>
+  )
+}
 
 export default Home
