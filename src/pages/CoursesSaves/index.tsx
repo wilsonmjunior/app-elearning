9293576e955e85d7f11b3ce389ac904d2defb3a0
wilsonmjunior <wilsonmjuniorx@gmailcom>
2020-12-22 17:19:14 -0300

    created the save courses page

--- src/pages/CoursesSaves/index.tsx
+++ src/pages/CoursesSaves/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { ActivityIndicator } from 'react-native'
 
-const CoursesSaves = () => (
-  <View>
-    <Text>Courses Saves</Text>
-  </View>
-)
+import Header from '../../components/Header'
+import ListCourses, { Course } from '../../components/ListCourses'
+import Title from '../../components/Title'
+
+import { useGet } from '../../hooks/api'
+import { normalize } from '../../utils/responsive'
+
+import { Container, Content, HeaderContent } from './styles'
+
+const CoursesSaves = () => {
+  const [data, loading] = useGet<Course>({ url: 'courses' })
+
+  return (
+    <Container>
+      <Header search />
+
+      <Content>
+        <HeaderContent>
+          <Title>Cursos Salvos</Title>
+        </HeaderContent>
+
+        {
+          !loading
+            ? <ListCourses courses={data} isDelete />
+            : <ActivityIndicator size="large" color="#6548A3" style={{ marginTop: normalize(100) }} />
+        }
+      </Content>
+    </Container>
+  )
+}
 
 export default CoursesSaves
---
+++ src/pages/CoursesSaves/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native'
+
+import { rem } from '../../utils/responsive'
+
+export const Container = styled.View``
+
+export const Content = styled.View`
+  margin-top: ${rem(-50)};
+  background-color: #F0EDF5;
+  width: 100%;
+  height: 100%;
+  border-radius: ${rem(24)};
+  z-index: 2;
+`
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  margin: 24px;
+  align-items: center;
+  justify-content: space-between;
+`
